ac5b6418e36a5f1719b9ba93f5c73fcebba45ef5
Hoang Dinh Nguyen <nguyen.hoang@sutrixsolutions.com>
2017-05-31 14:34:56 +0700

    commit new change

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import Header from './components/Header';
 import SidebarMenu from './components/SidebarMenu';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      height: 0,
+    }
+  }
+
+  componentDidMount() {
+    this.setState({height: window.innerHeight - 50 + 'px'});
+  }
+
   render() {
+    const { height  } = this.state;
     return (
       <div>
         <Header />
-        <SidebarMenu />
+        <SidebarMenu height={height}/>
       </div>
     );
   }
